9ed649013234341520d6d8316e30409de69d3e77
Breaker <791628659@qq.com>
2016-07-04 17:57:30 +0800

    练习1.16

---
+++ chapter-01/ex1_16.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int val, sum {0};
+    while(cin >> val)
+    {
+        sum += val;
+    }
+
+    cout << "The sum is:" << sum << endl;
+}
