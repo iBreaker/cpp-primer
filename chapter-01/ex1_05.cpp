75ee5659285b82fb194f1f32218d7ca55edb3757
Breaker <791628659@qq.com>
2016-07-04 15:50:39 +0800

    练习1.5

---
+++ chapter-01/ex1_05.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int v1, v2;
+
+    cout << "Enter two numbers:" ;
+    cout << endl;
+
+    cin >> v1; 
+    cin >> v2;
+
+    cout << "The product of ";
+    cout << v1 ;
+    cout << " and ";
+    cout << v2 ;
+    cout << " is: ";
+    cout << v1 * v2 ;
+    cout << endl;     
+    return 0;
+}
