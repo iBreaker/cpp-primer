694303c28a2a4de7bb371809971582b8f37e6aa4
Breaker <791628659@qq.com>
2016-07-05 12:07:57 +0800

    练习3.02

---
+++ chapter-03/ex3_02-1.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string line;
+
+    while(getline(cin, line))
+    {
+        cout << line << endl;
+    }
+}
---
+++ chapter-03/ex3_02-2.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string word;
+
+    while(cin >> word)
+    {
+        cout << word << endl;
+    }
+}
