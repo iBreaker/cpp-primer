f8de17eed0ff17c57d1e3a1ce1290ef828df3326
Breaker <791628659@qq.com>
2016-07-11 14:12:30 +0800

    练习3.31

---
+++ chapter-03/ex3_31.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int a[10] ;
+
+    for(int i = 0; i < 10; i++)
+    {
+        a[i] = i;
+        cout << a[i] << " " ;
+    }
+
+    cout << endl;
+    return 0;
+}
